e768a01dd0f57bbaff880196f732e07441dd3183
Alisa <7586237+alisaduncan@users.noreply.github.com>
2026-03-17 12:35:27 -0700

    chore: add v21 sample app

--- test/e2e/runner.cjs
+++ test/e2e/runner.cjs
@@ -60,6 +60,11 @@ const tasks = [
     app: 'angular-v20',
     asyncOktaConfig: true
   },
+  {
+    name: '@okta/test.app.ng21',
+    app: 'angular-v21',
+    asyncOktaConfig: true
+  },
 ]
   .reduce((tasks, app) => {
     const task = getTask(app);
